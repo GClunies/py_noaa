94f425894ee3a98703ef6b824659a6960e607fc7
Gregory Clunies <greg.clunies@gmail.com>
2018-02-11 15:39:23 -0800

    fix error in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='py_noaa',
       version='0.1',
-      description='Python module to fetch data from NOAA APIs,
+      description='Python module to fetch data from NOAA APIs',
       url='https://github.com/GClunies/py_noaa',
       author='Greg Clunies',
       author_email='greg.clunies@example.com',
