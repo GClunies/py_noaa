056df0e89d31337b0731a6b0494b80d38f470db3
Michael Delgado <delgado.michaelt@gmail.com>
2018-02-28 08:42:05 -0800

    add sample test file

---
+++ tests/test_py_noaa.py
@@ -0,0 +1,16 @@
+
+from __future__ import absolute_import
+from py_noaa import coops
+
+import pytest
+
+def test_error_handling():
+    with pytest.raises(ValueError):
+        coops.get_value(
+            begin_date="20150101",
+            end_date="20150331",
+            stationid="9442396",
+            product="water_level",
+            datum="navd88", # this is an invalid datum
+            units="metric",
+            time_zone="gmt")
