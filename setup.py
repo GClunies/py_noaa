99b27978133dc9b89ec55a68bf244eecdff162b9
Gregory Clunies <greg.clunies@gmail.com>
2018-03-06 19:35:33 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 
 setup(name='py_noaa',
-      version='0.2',
+      version='0.2.1',
       description='Python module to fetch data from NOAA APIs',
       url='https://github.com/GClunies/py_noaa',
       author='Greg Clunies',
       author_email='greg.clunies@gmail.com',
       license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7, 3.5, 3.6'
+          # Python sersions
+      ],
       packages=['py_noaa'],
       install_requires=['requests', 'numpy', 'pandas'],
       zip_safe=False)
